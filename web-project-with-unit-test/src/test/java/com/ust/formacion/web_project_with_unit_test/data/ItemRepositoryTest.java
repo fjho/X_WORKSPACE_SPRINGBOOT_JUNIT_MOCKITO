ee3dbbf670b7c62301c51e511274a7d39af2d0f7
Francisco Javier Hernandez Ochoa <francisco-javier.hern-ndez-ochoa@capgemini.com>
2025-11-12 12:46:38 +0100

    minor changes

--- web-project-with-unit-test/src/test/java/com/ust/formacion/web_project_with_unit_test/data/ItemRepositoryTest.java
+++ web-project-with-unit-test/src/test/java/com/ust/formacion/web_project_with_unit_test/data/ItemRepositoryTest.java
@@ -11,8 +11,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.core.env.Environment;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.TestPropertySource;
 
 import com.ust.formacion.web_project_with_unit_test.model.Item;
 import com.ust.formacion.web_project_with_unit_test.repositories.ItemRepository;
